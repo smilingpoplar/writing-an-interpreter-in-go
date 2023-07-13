6d8ff9f08858f6fdec0a2391137be75f90694abc
le <smilingpoplar@gmail.com>
2023-07-13 12:22:51 +0800

    1.4 #1

--- monkey/token/token.go
+++ monkey/token/token.go
@@ -17,8 +17,15 @@ const (
 	INT   = "INT"   // 1343456
 
 	// 运算符
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
 
 	// 分隔符
 	COMMA     = ","
