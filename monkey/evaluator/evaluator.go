72dd7c99ed36f5d74d73e5fa9f693b15cba55341
le <smilingpoplar@gmail.com>
2023-07-14 13:09:57 +0800

    3.5.3

--- monkey/evaluator/evaluator.go
+++ monkey/evaluator/evaluator.go
@@ -13,6 +13,8 @@ func Eval(node ast.Node) object.Object {
 		return Eval(node.Expression)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
 
 	return nil
@@ -26,4 +28,16 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
